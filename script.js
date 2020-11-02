3a8e4bea86f6c6da0c70c4866f3d8cd9ab207d93
Vladimir Larionov <narcisiwe@gmail.com>
2020-11-02 21:29:08 +0300

    Fix: Errors#04

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let number;
-
 const isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
@@ -19,29 +18,23 @@ const start = function() {
 function one() {
     const randomNum = Math.floor(Math.random() * 100 + 1);
     console.log(randomNum);
-function tryto(){
-function tryToWin() {
-    
-    
-start();
-function two(number) {
-    if (number > randomNum) {
-        alert ('Загаданное число меньше');
-    } else if (number < randomNum) {
-        alert ('Загаданное число больше');
-    } else {
-        alert ('Поздравляю, Вы угадали!!!');
-        return 0;
-    }
-        return randomNum, number;
-    }
 
+
+    function two() {
+        start();
+            
+        if (number > randomNum) {
+            alert ('Загаданное число меньше');
+            return two();
+        } else if (number < randomNum) {
+            alert ('Загаданное число больше');
+            return two();
+        } else {
+        return alert ('Поздравляю, Вы угадали!!!');
+        }
+       
+    }
     console.dir(two);
-    console.log(number);
-    return two (number);
-    
-}
-tryToWin();
-return tryto();
-} tryto();
-} one();
+    return two(number);
+} 
+one();
